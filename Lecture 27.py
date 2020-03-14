29a73691948f19045e1dd2eca646afe024fd9d2d
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-15 00:14:30 +0700

    add 28

--- Lecture 27.py
+++ Lecture 27.py
@@ -1,4 +1,10 @@
 x = 10
 y = 5
-z = 15
-print (x)
+z = x + y
+print (z)
+
+money = 150
+IncomePerDay = 300
+CostPerDay = 120
+Result = 150 + 300 * 30 - 120 * 30
+print (Result)
---
+++ Lecture 28.py
@@ -0,0 +1,11 @@
+x = True
+y = False
+z = x and y
+print(z)
+a = 10
+b = 5
+c = a > b
+print(c)
+age = 25
+print("สวัสดีครับ คุณอายุเกินกว่าที่จะเข้าร้านนี้ไหม ?")
+print(age>20)
