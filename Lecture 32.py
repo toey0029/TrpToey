deec39415debac58f77ff81e30a9461cdc8efeda
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-21 13:09:11 +0700

    add 32

---
+++ Lecture 32.py
@@ -0,0 +1,8 @@
+name = input("first name : ")
+lastname = input("last name : ")
+print("Hello",name,lastname)
+
+a = input("Number : ")
+b = input("Number : ")
+c = int(a)+int(b)
+print(a,"+",b,"=",(c))
