7492c397063b7c30b24dfe7c1236e455f4f32156
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-22 20:41:38 +0700

    add 39

---
+++ Lecture 39.py
@@ -0,0 +1,17 @@
+score = int(input("Glad : "))
+if score >= 80:
+    print("A")
+elif score >= 75:
+    print("B+")
+elif score >= 70:
+    print("B")
+elif score >= 65:
+    print("C+")
+elif score >= 60:
+    print("C")
+elif score >= 55:
+    print("D+")
+elif score >= 50:
+    print("D")
+elif score < 50:
+    print("F")
