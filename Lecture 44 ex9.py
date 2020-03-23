989b2e4a7d039b56b153bbce4b848be25c06ad20
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-23 17:26:19 +0700

    add

---
+++ Lecture 44 ex9.py
@@ -0,0 +1,7 @@
+user = input("Username : ")
+pas = input("Password : ")
+while user != ("toey") or pas != ("1234"):
+    print("Login Fail")
+    user = input("Username : ")
+    pas = input("Password : ")
+print("Login")
