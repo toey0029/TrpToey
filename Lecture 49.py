fd2166c528c93126d884b0d8e9922186596fc8a9
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-24 16:45:35 +0700

    d

---
+++ Lecture 49.py
@@ -0,0 +1,11 @@
+def hi():
+    print("Hello")
+    name()
+    print("Woooooo!!")
+def name():
+    print("Teerapob")
+    print("Haha")
+    hi()
+hi()
+name()
+
---
+++ Lecture 50.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x-y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x*y)
+addNumber(100,25)
+
+def addNumber(x,y):
+    print(x/y)
+addNumber(100,25)
