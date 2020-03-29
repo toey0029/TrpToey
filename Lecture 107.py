72428834c47a89a34640608dd6a28d1f3514c8a9
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-30 03:29:41 +0700

    a

---
+++ Lecture 107.py
@@ -0,0 +1,15 @@
+class Animal():
+    def eat(self):
+        print("Eating Eating!!!")
+
+class Cat(Animal):
+    __name = ""
+    def setName(self,text):
+        self.name = text
+        print("Setting New Cat Name =",self.name)
+    def eat(self):
+        print("Meaw !!",self.name)
+cat1=Cat()
+cat1.setName("TT")
+print(cat1.__name)
+cat1.eat()
